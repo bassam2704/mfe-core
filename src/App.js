95fe8bc216f4d84077223a4c6f21f75e220ac213
bassam2704 <mohammedbassam007@gmail.com>
2022-11-03 13:52:26 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React from 'react'
 // import {mount} from 'marketing/MarkertingApp' // mount is a simple function that give reference to the html element
 
 // console.log(mount)\
+import {StylesProvider,createGenerateClassName} from '@material-ui/core/styles'
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header'
 import MarketingApp from './components/MarketingApp'
+
+const generateClassname=createGenerateClassName({
+  productionPrefix:'core'
+})
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Header/>
-        <MarketingApp/>
-      </div>
-    </BrowserRouter>
-   
+    <StylesProvider generateClassName={generateClassname}>
+      <BrowserRouter>
+        <div>
+          <Header/>
+          <MarketingApp/>
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
 
