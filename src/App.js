b695e2cee0eddad7ba0f92c0a2ffbab8415a1f90
bassam2704 <mohammedbassam007@gmail.com>
2022-11-03 13:27:51 +0530

    added header file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 // import {mount} from 'marketing/MarkertingApp' // mount is a simple function that give reference to the html element
 
-// console.log(mount)
+// console.log(mount)\
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header'
 import MarketingApp from './components/MarketingApp'
 function App() {
   return (
-    <div>
-      <h1>HI THERE</h1>
-      <MarketingApp/>
-      <MarketingApp/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header/>
+        <MarketingApp/>
+      </div>
+    </BrowserRouter>
+   
   )
 }
 
